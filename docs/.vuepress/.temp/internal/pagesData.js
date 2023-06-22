db3ab2ad9676fffd1f5acabaa974cf2e59e25add
零零豆 <29398217@qq.com>
2023-06-23 03:04:40 +0800

    Delete docs directory

--- docs/.vuepress/.temp/internal/pagesData.js
+++
@@ -1,4 +0,0 @@
-export const pagesData = {
-  // path: /404.html
-  "v-3706649a": () => import(/* webpackChunkName: "v-3706649a" */"C:/Users/16653/Desktop/wiki/docs/.vuepress/.temp/pages/404.html.js").then(({ data }) => data),
-}
